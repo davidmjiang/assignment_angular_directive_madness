12cacd20cbaffc7c26e66edba5f12076b00ebd2a
David Jiang <davidjgrey@gmail>
2016-09-23 09:56:17 -0700

    header and copyright

--- app.js
+++ app.js
@@ -14,9 +14,33 @@ directiveMadness.directive('radioForm', function() {
   return {
     templateUrl: "radioForm.html",
     restrict: "E",
-    scope: {formValue: "="},
+    scope: {formValue: "@"},
     // link: function(scope) {
     //   scope.formValue = 2;
     // }
   }
-})
+});
+
+directiveMadness.directive('mainHeader',
+  function(){
+    return{
+      templateUrl: "mainHeader.html",
+      restrict: "E",
+      transclude: true,
+      scope:{}
+    }
+});
+
+directiveMadness.directive("copyright",
+  function(){
+    return{
+      templateUrl: "copyright.html",
+      restrict: "E",
+      transclude: true,
+      scope: {year: "@"},
+      link: function(scope){
+        scope.year = new Date().getFullYear();
+      }
+    }
+
+});
