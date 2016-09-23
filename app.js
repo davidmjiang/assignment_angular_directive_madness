183d389d8b6eb644913f52d9457725712aec04a6
Mike Lee <asackofwheat@gmail.com>
2016-09-23 12:07:11 -0400

    hidden

---
+++ app.js
@@ -0,0 +1,10 @@
+var directiveMadness = angular.module('directiveMadness', []);
+
+directiveMadness.directive('mainNav', function() {
+
+  return {
+    templateUrl: "mainNav.html",
+    restrict: "E",
+    scope: {hidden: "="}
+  };
+});
